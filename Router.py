596c0e00dacdbaa5f212d409b487a16750530ce6
cpf5193 <cpf5193@cs.washington.edu>
2015-03-02 00:44:40 -0800

    Initial design of Router and RouterConnection relationship

--- Router.py
+++ Router.py
@@ -1,15 +1,18 @@
-# RoutingTable
-# TimerTable
-# Connections
-# Initializer to run on prompting from the node class
-# Register function
+import CommandCellOpen, RelayCell, Cell, RouterConnection
 
 class Router(object):
-  def __init__(self):
+  #############################################
+  ## Constructor
+  #############################################
+  def __init__(self, converter):
     self.routingTable = {}
     self.timers = {}
     self.connections = {}
+    self.converter = converter
 
+  #############################################
+  ## Router startup functions
+  #############################################
   def registerRouter():
     print "Registering router (incomplete)"
     #register with the registration service here
@@ -18,24 +21,86 @@ class Router(object):
     print "Creating circuit (incomplete)"
     #connect to three other routers and create a circuit
 
+  def manageTimeouts():
+    #create a new thread to manage the timer table,
+    #checking if circuits have timed out and calling destroy if so
+    print "Creating timeout manager (incomplete)"
+
   def start():
     registerRouter()
     createCircuit()
     #accept incoming connections here
     print "Starting the router (incomplete)"
 
+
+  #############################################
+  ## Reading/Delegating functions
+  #############################################
   def handleProxyMessage():
     # Take a message from the converter and place it into the appropriate
-    # buffer
+    # buffer using RouterConnection's writeToBuffer
     print "Handling message from proxy side (incomplete)"
 
-  def handleRouterMessage():
-    # Take a message from the buffer, process it, and send it either to
+  def handleRouterMessage(msg):
+    # Accept a message from a RouterConnection, process it, and send it either to
     # the converter or to another router connection's buffer
-    print "Handling message from router buffer (incomplete"
+    print "Handling message from router buffer (incomplete)"
+    # figure out what kind of message this is and delegate the processing
+    # to one of the Handle Message functions
 
+  #############################################
+  ## Destruction functions
+  #############################################
   def destroyCircuit(circuitId):
     # Destroy the specified circuit
-    print "Destroying circuit #%x" % circuitId
-
+    print "Destroying circuit #%x (incomplete)" % circuitId
   
+
+  #############################################
+  ## Handle Message Functions
+  #############################################
+  def handleOpen():
+    # The received cell is an Open cell, do appropriate logic
+    print "handling Open message (incomplete)"
+
+  def handleOpened():
+    # The received cell is an Opened cell, do appropriate logic
+
+  def handleOpenFailed():
+    # The received cell is an Open Failed cell, do appropriate logic
+
+  def handleCreate():
+    # The received cell is a Created cell, do appropriate logic
+
+  def handleCreated():
+    # The received cell is a Created cell, do appropriate logic
+
+  def handleCreateFailed():
+    # The received cell is a Create Failed cell, do appropriate logic
+
+  def handleDestroy():
+    # The received cell is a Destroy cell, do appropriate logic
+
+  def handleBegin():
+    # The received cell is a relay begin cell, do appropriate logic
+
+  def handleData():
+    # The received cell is a relay data cell, do appropriate logic
+
+  def handleEnd():
+    # The received cell is a relay end cell, do appropriate logic
+
+  def handleConnected():
+    # The received cell is a relay connected cell, do appropriate logic
+
+  def handleExtend():
+    # The received cell is a relay extend cell, do appropriate logic
+
+  def handleExtended():
+    # The received cell is a relay extended cell, do appropriate logic
+
+  def handleBeginFailed():
+    # The received cell is a relay Begin Failed cell, do appropriate logic
+
+  def handleExtendFailed():
+    # The received cell is a relay Extend Failed cell, do appropriate logic
---
+++ RouterConnection.py
@@ -0,0 +1,39 @@
+import Router
+
+class RouterConnection(object):
+  def __init__(self, router, circuitId, port):
+    self.buffer = {} #some type of threadsafe queue
+    self.circuitId = circuitId
+    self.router = router # The router that this RouterConnection is a part of
+    self.port = port
+    #start the reader and writer threads
+    # e.g. readFromRouter() && readFromBuffer()
+
+  #############################################
+  ## Connection Functions
+  #############################################
+  def connectToRouter(circuitId, port):
+    # do a tcp connect to the specified router
+
+  def disconnectFromRouter(hostname, port):
+    # destroy the tcp connection to this router
+
+  #############################################
+  ## Read/Write Functions
+  #############################################
+  def writeToBuffer(msg):
+    #insert msg into buffer
+
+  # Read from the remote router
+  def readFromRouter():
+    # read from the router and call the router's function to process the message
+    handleRouterMessage(msg)
+    # (origin router should process the message and decide what to do with it)
+
+  def readFromBuffer():
+    # Take a queued message from the buffer and write to the Router
+    writeToRouter(msg)
+
+  # Write to the remote router
+  def writeToRouter(msg):
+    # send the indicated message to the router

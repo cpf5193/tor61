c99b125fba18b5bfe5482600b1f3da320b635d4e
cpf5193 <cpf5193@cs.washington.edu>
2015-03-01 23:13:50 -0800

    update router skeleton

--- Router.py
+++ Router.py
@@ -11,11 +11,11 @@ class Router(object):
     self.connections = {}
 
   def registerRouter():
-    print "Registering router (inactive)"
+    print "Registering router (incomplete)"
     #register with the registration service here
 
   def createCircuit():
-    print "Creating circuit"
+    print "Creating circuit (incomplete)"
     #connect to three other routers and create a circuit
 
   def start():
@@ -23,5 +23,19 @@ class Router(object):
     createCircuit()
     #accept incoming connections here
     print "Starting the router (incomplete)"
+
+  def handleProxyMessage():
+    # Take a message from the converter and place it into the appropriate
+    # buffer
+    print "Handling message from proxy side (incomplete)"
+
+  def handleRouterMessage():
+    # Take a message from the buffer, process it, and send it either to
+    # the converter or to another router connection's buffer
+    print "Handling message from router buffer (incomplete"
+
+  def destroyCircuit(circuitId):
+    # Destroy the specified circuit
+    print "Destroying circuit #%x" % circuitId
 
   

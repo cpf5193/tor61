8f36223fef92a9b65b8134569ebb6c3bae029890
cpf5193 <cpf5193@cs.washington.edu>
2015-03-09 19:38:54 -0700

    Initial implementation of handleOpen

--- Router.py
+++ Router.py
@@ -6,16 +6,39 @@ class Router(object):
   #############################################
   ## Constructor
   #############################################
+  CMDS = {
+    0x01: handleCreate,
+    0x02: handleCreated,
+    0x03: handleRelay,
+    0x04: handleDestroy,
+    0x05: handleOpen,
+    0x06: handleOpened,
+    0x07: handleOpenFailed,
+    0x08: handleCreateFailed
+  }
+
+  RELAY_CMDS = {
+    0x01: handleBegin,
+    0x02: handleData,
+    0x03: handleEnd,
+    0x04: handleConnected,
+    0x06: handleExtend,
+    0x07: handleExtended,
+    0x0b: handleBeginFailed,
+    0x0c: handleExtendFailed
+  }
+
+  NEXT_CIRC_ID = 0x0003
+
   def __init__(self, converter, groupNum, instanceNum, port):
     self.routingTable = {}
     self.timers = {}
-    self.connections = {}
+    self.connections = {} #format: (ip, port) --> RouterConnection
     self.converter = converter
     self.groupNum = groupNum
     self.instanceNum = instanceNum
     self.port = port
     self.agentId = (groupNum << 16) | instanceNum
-    self.circuitStatus = "Not Started"
 
   #############################################
   ## Router startup functions
@@ -29,31 +52,40 @@ class Router(object):
 
   def createCircuit(self):
     print "Creating circuit (incomplete)"
-    #connect to three other routers and create a circuit
+    # Find three other routers and create a circuit
     peers = self.getPeers()
 
     # Create a new connection between this router and the first peer
     conn = RouterConnection.RouterConnection(self, 0x0000, peers[0]['ip'], peers[0]['port'])
     conn.connectToRouter()
 
-    #Add this connection to the table
-    connections[peers[0]['ip'] + ':' + peers[0]['port']] = conn
+    # Add this connection to the table
+    connections[(peers[0]['ip'], peers[0]['port'])] = conn
     
-    #Create an OPEN cell to send to the first peer
+    # Send an OPEN cell to the first peer
     self.doOpen(conn)
     
-    #Send a CREATE cell
+    # Send a CREATE cell
     self.doCreate(conn)
     
-    #Send a RELAY EXTEND cell x2 using the stream id 0x0000 
-    #and the circuit id 0x0001
+    # Send a RELAY EXTEND cell x2 using the stream id 0x0000 
+    # and the circuit id 0x0001
     self.doExtend(conn, peers[1])
     self.doExtend(conn, peers[2])
 
     print "Successfully created circuit with id 0x0001"
     
-    #TODO: Start listening for other connections once the circuit is created
-    
+    # Start listening for other connections once the circuit is created
+    connectionAccepter = socket.socket(AF_INET, SOCK_STREAM)
+    host = socket.getHostName()
+    port = self.port
+    socket.bind((host, port))
+    socket.listen(5)
+    while(True):
+      (conn, address) = socket.accept()
+      rc = RouterConnection.RouterConnection(self, nextCircId, address[0], address[1], conn)
+      NEXT_CIRC_ID += 2
+      connections[address] = rc
   
   def getPeers(self):
     print "fetching registered routers"
@@ -62,7 +94,7 @@ class Router(object):
 
     process = subprocess.Popen([string], stdout = subprocess.PIPE, shell=True)
     (output, err) = process.communicate()
-
+    
     peers = output.splitlines()
     for i in range(0, len(peers)):
       peers[i] = peers[i].split('\t')
@@ -84,11 +116,23 @@ class Router(object):
 
   def start(self):
     self.registerRouter()
-    time.sleep(3)
+    time.sleep(2) #Allow time for the registration to happen
+    #Should try to make sure there are available peers
     self.createCircuit()
-    #accept incoming connections here
-    print "Starting the router (incomplete)"
-    return
+    
+    print "Starting the router"
+    # Start listening for other connections once the circuit is created
+    connectionAccepter = socket.socket(AF_INET, SOCK_STREAM)
+    host = socket.getHostName()
+    port = self.port
+    socket.bind((host, port))
+    socket.listen(5)
+    circuitId = 0x0003
+    while(True):
+      (conn, address) = socket.accept()
+      rc = RouterConnection.RouterConnection(self, circuitId, address[0], addres\
+s[1], conn)
+      connections[(circuitId, address)] = rc
 
   #############################################
   ## Reading/Delegating functions
@@ -98,12 +142,25 @@ class Router(object):
     # buffer using RouterConnection's writeToBuffer
     print "Handling message from proxy side (incomplete)"
 
-  def handleRouterMessage(self, msg):
+  def handleRouterMessage(self, msg, remoteIp, remotePort):
     # Accept a message from a RouterConnection, process it, and send it either to
     # the converter or to another router connection's buffer
     print "Handling message from router buffer (incomplete)"
-    # figure out what kind of message this is and delegate the processing
-    # to one of the Handle Message functions
+
+    # Extract the cell type from the Cell
+    cell = Cell.setBuffer(msg)
+    circuitId = cell.getCircuitId()
+    cmdId = cell.getCmdId()
+
+    # Call the handler method associated with the specified command
+    if (cmdId == 0x03):
+      relayCell = RelayCell.setBuffer(msg)
+      relayCmd = relayCell.getRelayCmd()
+      RELAY_CMDS[relayCmd](msg)
+    elif (cmdId == 0x05):
+      CMDS[cmdId](msg, remoteIp, remotePort)
+    else:
+      CMDS[cmdId](msg)
 
   #############################################
   ## Destruction functions
@@ -133,14 +190,13 @@ class Router(object):
     else:
       print "ERROR: unexpected command type in response to OPEN"
       sys.exit(1)
-    # The received cell is an Opened cell, do appropriate logic
 
   def doCreate(self, connection):
     createMsg = Cell.Cell(0x0001, 0x01)
     connection.writeToBuffer(createMsg.getBuffer())
     reply = connection.readFromRouter()
     replyMsg = Cell.setBuffer(reply)
-    msgType = replyMsg.getCmdId()
+a    msgType = replyMsg.getCmdId()
     if (msgType == 0x02):
       print "CREATED cell received"
       #Add the mapping of (circuitId, (ip:port)) --> (circuitId, (ip:port))
@@ -179,57 +235,71 @@ class Router(object):
   #############################################
   ## Handle Message Functions
   #############################################
-  def handleOpen(self, msg):
-    return
+  def handleOpen(self, msg, remoteIp, remoteHost):
+    # If we receive an open cell, check for an open tcp connection
+    # to the specified router and if it exists send back an 'Opened' cell
+    cell = CommandCellOpen.setBuffer(msg)
+    remoteHost = cell.getOpenerId()
+    localHost = cell.getOpenedId()
+    if (localHost != self.agentId):
+      print "Open cell received by wrong host"
+      sys.exit(1)
+    elif (remoteIp, remoteHost) not in connections:
+      print "Open request sent on invalid connection"
+      sys.exit(1)
+    else:
+      # Generate an OPENED cell and send over the connection
+      openedCell = CommandCellOpen.CommandCellOpen(0x0000, 0x06, remoteHost, localHost)
+      connections[(remoteIp, remoteHost)].writeToBuffer(openedCell.getBuffer())
   
-  def handleOpenFailed(self):
+  def handleOpenFailed(self, msg):
     return
     # The received cell is an Open Failed cell, do appropriate logic
 
-  def handleCreate(self):
+  def handleCreate(self, msg):
     # The received cell is a Created cell, do appropriate logic
     return
 
-  def handleCreated(self):
+  def handleCreated(self, msg):
     return
     # The received cell is a Created cell, do appropriate logic
 
-  def handleCreateFailed(self):
+  def handleCreateFailed(self, msg):
     # The received cell is a Create Failed cell, do appropriate logic
     return
 
-  def handleDestroy(self):
+  def handleDestroy(self, msg):
     # The received cell is a Destroy cell, do appropriate logic
     return
 
-  def handleBegin(self):
+  def handleBegin(self, msg):
     # The received cell is a relay begin cell, do appropriate logic
     return
 
-  def handleData(self):
+  def handleData(self, msg):
     # The received cell is a relay data cell, do appropriate logic
     return
 
-  def handleEnd(self):
+  def handleEnd(self, msg):
     # The received cell is a relay end cell, do appropriate logic
     return
 
-  def handleConnected(self):
+  def handleConnected(self, msg):
     # The received cell is a relay connected cell, do appropriate logic
     return
 
-  def handleExtend(self):
+  def handleExtend(self, msg):
     # The received cell is a relay extend cell, do appropriate logic
     return
 
-  def handleExtended(self):
+  def handleExtended(self, msg):
     # The received cell is a relay extended cell, do appropriate logic
     return
 
-  def handleBeginFailed(self):
+  def handleBeginFailed(self, msg):
     # The received cell is a relay Begin Failed cell, do appropriate logic
     return
 
-  def handleExtendFailed(self):
+  def handleExtendFailed(self, msg):
     # The received cell is a relay Extend Failed cell, do appropriate logic
     return
--- RouterConnection.py
+++ RouterConnection.py
@@ -2,26 +2,20 @@ import Router
 import Queue, time
 
 class RouterConnection(object):
-  def __init__(self, router, circuitId, ip, port):
+  def __init__(self, router, circuitId, ip, port, socket):
     self.buffer = Queue(100000)
     self.circuitId = circuitId
     self.router = router # The router that this RouterConnection is a part of
     self.ip = ip
     self.port = port
+    self.socket = socket
+    self.startThreads()
   
   #############################################
   ## Connection Functions
   #############################################
-  def connectToRouter(self):
-    # do a tcp connect to the specified router
-    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    errno = tcpSocket.connect_ex(self.ip, self.port)
-    if not errno == 0:
-      print "Failed to create tcp connection to first peer"
-      sys.exit(1)
-    print "TCP Connection created to %s:%s" % (self.ip, self.port)
-    self.socket = tcpSocket
-
+  
+  def startThreads(self):
     #start the reader and writer threads
     # e.g. readFromRouter() && readFromBuffer()
     fromRouter = threading.Thread(target=readFromRouter, args=())
@@ -44,7 +38,7 @@ class RouterConnection(object):
   def readFromRouter():
     while(True):
       routerMsg = self.socket.recv(Cell.LENGTH)
-      router.handleRouterMessage(routerMsg)
+      router.handleRouterMessage(routerMsg, self.ip, self.port)
       # (origin router should process the message and decide what to do with it)
 
   def readFromBuffer():
@@ -63,7 +57,6 @@ class RouterConnection(object):
   # Write to the remote router
   def writeToRouter(msg):
     # send the indicated message to the router
-    # TODO: add try/catch
     try:
       self.socket.sendAll(msg)
     except:
